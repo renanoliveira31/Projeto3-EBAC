2b19a60b57c75086a70307fc44198c4cdd778e5d
RENAN FELICIANO OLIVEIRA <renanfeoliveira1@gmail.com>
2024-03-09 11:35:50 -0300

    terminado html css e js para HD

---
+++ main.js
@@ -0,0 +1,5 @@
+    $(document).ready(function(){
+        $('#email').attr('placeholder', 'exemple@exemple.com');
+        $('#telefone').attr('placeholder', '+55 (00) 00000-0000');
+        $('#telefone').mask('+55(00) 00000-0000');
+    });
